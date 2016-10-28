383f61f7658d0439b18a0a1a27ecb96fb54ba3ff
piotrq18 <piotrq18@gmail.com>
2016-10-29 01:00:49 +0200

    Add Timer App

--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,9 +1,75 @@
 var React = require('react');
+var Clock = require('Clock');
+var Controls = require('Controls');
 
 var Timer = React.createClass({
+  getInitialState: function(){
+    return {
+      count: 0,
+      countdownStatus: 'paused'
+    }
+  },
+  componentDidUpdate: function(prevProps, prevState){
+    if (this.state.countdownStatus !== prevState.countdownStatus){
+      switch(this.state.countdownStatus){
+        case 'started':
+          this.startTimer();
+          break;
+        case 'stopped':
+          this.setState({
+            count: 0
+          })
+        case 'paused':
+          clearInterval(this.timer);
+          this.timer = undefined;
+          break;
+      }
+    }
+  },
+  componentWillUnmount: function(){
+    clearInterval(this.timer);
+    this.timer = undefined;
+  },
+  startTimer: function(){
+    this.timer = setInterval(() => {
+      var newCount = this.state.count + 1;
+      this.setState({
+        count: newCount
+      });
+
+      if(newCount === 0){
+        this.setState({
+          countdownStatus: 'stopped'
+        })
+      }
+    }, 1000);
+  },
+  handleStatusChange: function(newStatus){
+    this.setState({
+      countdownStatus: newStatus
+    });
+  },
+  handleSetCountdown: function(seconds){
+    this.setState({
+      count: seconds,
+      countdownStatus: 'started'
+    });
+  },
   render: function(){
+    var {count, countdownStatus} = this.state;
+    var renderControlAeria = () => {
+      if (countdownStatus !== 'stopped'){
+        return <Controls countdownStatus={countdownStatus} onStatusChange={this.handleStatusChange}/>
+      }else{
+        return <Controls countdownStatus={'paused'} onStatusChange={this.handleStatusChange}/>
+      }
+    };
     return (
-      <p>Timer.jsx loaded</p>
+      <div>
+        <h1 className="page-title">Timer app</h1>
+        <Clock totalSeconds={count}/>
+        {renderControlAeria()}
+      </div>
     )
   }
 });
